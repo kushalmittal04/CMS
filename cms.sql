9503ecf58b4b352cb26f26a797a8c977f6dd2dcb
kushalmittal04 <162821581+kushalmittal04@users.noreply.github.com>
2025-04-21 21:23:46 +0530

    Database File

---
+++ cms.sql
@@ -0,0 +1,150 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 22, 2025 at 05:19 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `hashed_password` char(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `first_name`, `last_name`, `email`, `username`, `hashed_password`) VALUES
+(1, 'Kushal', 'Mittal', 'kushalmittal04@gmail.com', 'Kushal2022', '$2y$10$95jkk96eNMAQzBmDIjeMGuE97BmiwAuBurtV7baJAnBYiTZBJU3.a'),
+(2, 'Chandan', 'Mittal', 'chandanmittal04@gmaail.com', 'Chandan2022', '$2y$10$1TSkEWZ4ctxq4wSzreYjRu4FTlCl56Vzo5Esry9vtMz0tr2yAwovG');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pages`
+--
+
+CREATE TABLE `pages` (
+  `id` int(11) NOT NULL,
+  `subject_id` int(11) DEFAULT NULL,
+  `menu_name` varchar(255) DEFAULT NULL,
+  `position` int(3) DEFAULT NULL,
+  `visible` tinyint(1) DEFAULT NULL,
+  `content` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pages`
+--
+
+INSERT INTO `pages` (`id`, `subject_id`, `menu_name`, `position`, `visible`, `content`) VALUES
+(1, 1, 'Globe Bank', 1, 1, NULL),
+(2, 1, 'History', 2, 1, NULL),
+(3, 1, 'Leadership', 3, 1, NULL),
+(4, 1, 'Contact Us', 4, 1, NULL),
+(5, 2, 'Banking', 1, 1, NULL),
+(6, 2, 'Credit Cards', 2, 1, NULL),
+(7, 2, 'Mortgages', 3, 1, NULL),
+(8, 3, 'Checking', 1, 1, NULL),
+(9, 3, 'Loans', 2, 1, NULL),
+(10, 3, 'Merchant Services', 3, 1, NULL),
+(11, 5, 'Financing', 1, 1, '<section id=\"blog\">\r\n  <h2>Recent Articles</h2>\r\n  <article>\r\n    <h3>Getting Started with PHP and MySQL</h3>\r\n    <p>Learn how to set up your first project and integrate a database seamlessly.</p>\r\n    <a href=\"/blog/php-mysql-tutorial\">Read More</a>\r\n  </article>\r\n  <article>\r\n    <h3>Building a CMS: Step-by-Step Guide</h3>\r\n    <p>A comprehensive guide inspired by LinkedIn Learning.</p>\r\n    <a href=\"/blog/building-cms\">Read More</a>\r\n  </article>\r\n</section>');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subjects`
+--
+
+CREATE TABLE `subjects` (
+  `id` int(11) NOT NULL,
+  `menu_name` varchar(255) DEFAULT NULL,
+  `position` int(3) DEFAULT NULL,
+  `visible` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `subjects`
+--
+
+INSERT INTO `subjects` (`id`, `menu_name`, `position`, `visible`) VALUES
+(1, 'About Globe Bank', 1, 1),
+(2, 'Consumer', 2, 1),
+(3, 'Small Business', 3, 1),
+(5, 'Commercial', 4, 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pages`
+--
+ALTER TABLE `pages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_subject_id` (`subject_id`);
+
+--
+-- Indexes for table `subjects`
+--
+ALTER TABLE `subjects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `pages`
+--
+ALTER TABLE `pages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `subjects`
+--
+ALTER TABLE `subjects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
